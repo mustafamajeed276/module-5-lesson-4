0b4490645d8cbd186bdaa98375ee6ec57e345982
mustafa276 <mmustafamajeed1@gmail.com>
2024-10-28 13:52:09 +0500

    pricey computer

---
+++ activity2.py
@@ -0,0 +1,13 @@
+class gamingcomp:
+    def __init__(self):
+        self.__maxprice = 900
+    def sell(self):
+        print("selling price : {}".format(self.__maxprice))
+    def setmaxprice(self, price):
+        self.__maxprice = price
+c = gamingcomp()
+c.sell()
+c.__maxprice = 1000
+c.sell()
+c.setmaxprice(1000)
+c.sell()
